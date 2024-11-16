e36e126722e6decb11e7f5d1273ece8f5ec44bcc
safsaf1533@gmail.com <safsaf1533@gmail.com>
2024-11-16 18:53:12 +0300

    docker flex, пока только падали...

--- app/models/config.py
+++ app/models/config.py
@@ -11,8 +11,5 @@ class Config(BaseSettings):
     secret_key: str
     algorithm: str
     access_token_expire_minutes: int
-    ACCESS_KEY_ID: str
-    SECRET_ACCESS_KEY: str
-    PHOTO_BUCKET: str
 
 config = Config(_env_file='.env', _env_file_encoding='utf-8')
