e5d36c7bcf7aa2c62653db2eef7b39e2c3e865c3
Timofey Markin <65281674+d-exterus@users.noreply.github.com>
2024-11-16 21:21:21 +0300

    docker flex, теперь он встал

--- app/schemas/others.py
+++ app/schemas/others.py
@@ -19,7 +19,7 @@ class EmployeeBase(BaseModel):
     resume_url: Optional[str] = None
     bio: str
     
-    @field_validator("phone_number")
+    @field_validator("number")
     def validate_phone_number(cls, value: str) -> str:
         if not re.match(r'^\+\d{5,15}$', value):
             raise ValueError('Номер телефона должен начинаться с "+" и содержать от 5 до 15 цифр')
